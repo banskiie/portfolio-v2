00f558180349ca905e2889ca5ddb66f4e23bad89
Banskiie <ivansinohon28@gmail.com>
2023-05-06 13:16:01 +0800

    fixed responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ import { motion } from "framer-motion";
 function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <section>
+      <section className="flex flex-col items-center justify-center">
         <div className="py-6 md:py-12">
           <motion.img
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { delay: 0.4, duration: 1 } }}
-            className="h-72 w-72 md:h-96 md:w-96 object-cover rounded-full"
+            className="h-80 w-80 md:h-[30rem] md:w-[30rem] object-cover rounded-full"
             src={mainImage}
             alt="A picture of me, Shand Ivan."
           />
@@ -23,7 +23,7 @@ function Home() {
               y: 0,
               transition: { delay: 1, duration: 1.5, type: "tween" },
             }}
-            className="text-primary"
+            className="text-primary text-center text-lg md:text-3xl"
           >
             Hi! I'm Shand Ivan P. Sinohon.
           </motion.p>
